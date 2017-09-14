c97055fdf9954fb948ce7e729e27893d40a1aff0
william <william@dhcppc5>
2017-09-13 22:55:58 -0600

    Se añadido el metodo capitalizar

--- src/main/java/com/mycompany/g1/Utilities.java
+++ src/main/java/com/mycompany/g1/Utilities.java
@@ -8,6 +8,59 @@ String modificar;
 modificar=texto.substring(0,30);
 texto=modificar;
 return texto;
-}    
+}
+
+//El texto que se ha recibido con cada palabra capitalizada
+public String Capitalizar(String texto){
+       String salida = "";
+        String modificado;
+        int num;
+        modificado = texto;
+        num = texto.length();
+        String[] key = new String[num];
+
+//descomponiendo la frase
+        for (int i = 0; i < num; i++) {
+            modificado = texto.substring(i, num);
+            key[i] = modificado.substring(0, 1);
+        }
+
+//convertir todo minusculas        
+        for (int i = 0; i < num; i++) {
+                key[i]=key[i].substring(0, 1).toLowerCase();
+        }
+//convertir despues de los espacios en mayusculas y la primera letra tambien        
+           for (int i = 0; i < num; i++) {
+   
+            if (key[i].equals(" ")) {
+                key[i + 1] = key[i + 1].substring(0, 1).toUpperCase();
+            } 
+
+            if (i == 0) {
+                key[i] = key[i].substring(0, 1).toUpperCase();
+            }
+
+        }
+//eliminar espacios dobles   
+int cont=0;           
+for (int i = 0; i < num; i++) {
+   
+            if (key[i].equals(" ")) {
+            cont=cont+1;    
+            } 
+            else{
+            cont=0;
+            }
+            if(cont<2){
+            salida = salida + key[i];
+            }
+            
+        }
+
+texto=salida; 
+    
+return texto;    
+}
+
     
 }
